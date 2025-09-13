4287dba48aa3192b004b1ec649d77f0f3760758f
Folu <ibukunmifoluke@gmail.com>
2025-09-13 02:05:55 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+# setup local packages (us_visa)
+setup(
+    name ="us_visa",
+    version = "0.0.0",
+    author = "Foluke",
+    author_email="ibukunmifoluke@gmail.com",
+    packages=find_packages()
+)
